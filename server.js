33ab4e10a0b7ef1824b5ece4c2a8eace04688601
Marco <marco.costa@aluno.ufop.edu.br>
2020-07-19 00:01:16 -0300

    Cors

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const requireDir = require('require-dir');
 const cors = require('cors')
 //App start
 const app = express();
+var corsOptions = {
+  origin: 'https://application-tcc.herokuapp.com/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
