45f6cd76d205ef2c8743d2626575397efe635987
Marco Antônio de Oliveira Costa <marco@yardim.com.br>
2020-12-21 23:16:04 -0300

    .

--- server.js
+++ server.js
@@ -2,21 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const requireDir = require('require-dir');
-const cors = require('cors')
+const requireDir = require("require-dir");
 //App start
 const app = express();
-var corsOptions = {
-  origin: 'https://application-tcc.herokuapp.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
 //app.use(cros());
 app.use(express.json());
 app.use(bodyParser.json());
 //DB start
-mongoose.connect(process.env.URL_MONGO, { useUnifiedTopology: true, useNewUrlParser: true })
-requireDir('./src/models/');
+mongoose.connect(process.env.URL_MONGO, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+requireDir("./src/models/");
 
-app.use('/api', require('./src/routes'));
+app.use("/api", require("./src/routes"));
 
-app.listen(process.env.PORT||3001);
+app.listen(process.env.PORT || 3001);
