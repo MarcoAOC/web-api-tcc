8ebb66519654f3926c04b1b92a324eefb5895c03
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2021-09-01 17:35:58 -0300

    teste

--- src/controllers/InfoController.js
+++ src/controllers/InfoController.js
@@ -1,32 +1,30 @@
-const mongoose = require('mongoose');
-const Mote = mongoose.model('Mote');
-const SensorData = mongoose.model('SensorData');
+const mongoose = require("mongoose");
+const Mote = mongoose.model("Mote");
+const SensorData = mongoose.model("SensorData");
 
 module.exports = {
-    async getQntyInfos(req,res){
-        const userId = req.userId;
-        const motes = await Mote.find({
-            "userId" : userId
-        });
-        if(motes.length<=0){
-            return res.status(400).json({ error: "Motes not founded" })
-        }
-        var motesQntty = motes.length;
-        var sensorsQntty =0;
-        var actuatorsQntty =0;
-        for(mote of motes){
-            const sensorData = await SensorData.findOne(
-                {
-                    "moteNetId": mote.moteNetId,
-                    "userId" : userId
-                }
-            ).sort({ created_at: 'asc', _id: -1 });
-            if(sensorData){
-
-                sensorsQntty+=(sensorData.sensors && sensorData.sensors.length) || 0;
-                actuatorsQntty+=(sensorData.actuators && sensorData.actuators.length) || 0;
-            }
-        }
-        return res.json({sensorsQntty,motesQntty,actuatorsQntty});
+  async getQntyInfos(req, res) {
+    const userId = req.userId;
+    const motes = await Mote.find({
+      userId: userId,
+    });
+    if (motes.length <= 0) {
+      return res.status(400).json({ error: "Motes not found" });
     }
-}
+    var motesQntty = motes.length;
+    var sensorsQntty = 0;
+    var actuatorsQntty = 0;
+    for (mote of motes) {
+      const sensorData = await SensorData.findOne({
+        moteNetId: mote.moteNetId,
+        userId: userId,
+      }).sort({ created_at: "asc", _id: -1 });
+      if (sensorData) {
+        sensorsQntty += (sensorData.sensors && sensorData.sensors.length) || 0;
+        actuatorsQntty +=
+          (sensorData.actuators && sensorData.actuators.length) || 0;
+      }
+    }
+    return res.json({ sensorsQntty, motesQntty, actuatorsQntty });
+  },
+};
